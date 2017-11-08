929cecfc59bba555755b876815f747f811838fe5
kira <kizxm@yahoo.com>
2017-11-08 15:26:03 -0800

    brush settings and mainactivity changes

--- app/src/main/java/com/kizxm/kirakiradraw/models/Brush.java
+++ app/src/main/java/com/kizxm/kirakiradraw/models/Brush.java
@@ -1,5 +1,20 @@
 package com.kizxm.kirakiradraw.models;
 
+import android.graphics.Path;
+
 public class Brush {
 
+    public int color;
+    public boolean emboss;
+    public boolean blur;
+    public int strokeW;
+    public Path path;
+
+    public Brush(int color, boolean emboss, boolean blur, int strokeW, Path path) {
+        this.color = color;
+        this.emboss = emboss;
+        this.blur = blur;
+        this.strokeW = strokeW;
+        this.path = path;
+    }
 }
