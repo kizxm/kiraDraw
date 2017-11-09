efe291579beb636b18f7852cf17c137fb93ba9c4
kira <kizxm@yahoo.com>
2017-11-08 16:22:00 -0800

    canvas view

--- app/src/main/java/com/kizxm/kirakiradraw/MainActivity.java
+++ app/src/main/java/com/kizxm/kirakiradraw/MainActivity.java
@@ -1,118 +1,50 @@
 package com.kizxm.kirakiradraw;
 
 
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BlurMaskFilter;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.EmbossMaskFilter;
-import android.graphics.MaskFilter;
-import android.graphics.Paint;
-import android.graphics.Path;
-import android.util.AttributeSet;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
-import com.kizxm.kirakiradraw.models.Brush;
+public class MainActivity extends AppCompatActivity {
+    private CanvasView canvasView;
 
-import java.util.ArrayList;
-
-public class MainActivity extends View {
-
-    public static int BRUSH_SIZE = 15;
-    public static final int DEFAULT_COLOR = Color.BLUE;
-    public static final int DEFAULT_BG_COLOR = Color.WHITE;
-    private static final float TOUCH_TOLERANCE = 4;
-    private float mX, mY;
-    private Path mPath;
-    private Paint mPaint;
-    private ArrayList<Brush> paths = new ArrayList<>();
-    private int currentColor;
-    private int backgroundColor = DEFAULT_BG_COLOR;
-    private int strokeWidth;
-    private boolean emboss;
-    private boolean blur;
-    private MaskFilter mEmboss;
-    private MaskFilter mBlur;
-    private Bitmap mBitmap;
-    private Canvas mCanvas;
-    private Paint mBitmapPaint = new Paint(Paint.DITHER_FLAG);
-
-    public MainActivity(Context context) {
-        this(context, null);
-    }
-
-    public MainActivity(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        mPaint = new Paint();
-        mPaint.setAntiAlias(true);
-        mPaint.setDither(true);
-        mPaint.setColor(DEFAULT_COLOR);
-        mPaint.setStyle(Paint.Style.STROKE);
-        mPaint.setStrokeJoin(Paint.Join.ROUND);
-        mPaint.setStrokeCap(Paint.Cap.ROUND);
-        mPaint.setXfermode(null);
-        mPaint.setAlpha(0xff);
-
-        mEmboss = new EmbossMaskFilter(new float[]{1, 1, 1}, 0.4f, 6, 3.5f);
-        mBlur = new BlurMaskFilter(5, BlurMaskFilter.Blur.NORMAL);
-
-    }
-
-    public void init(DisplayMetrics metrics) {
-        int height = metrics.heightPixels;
-        int width = metrics.widthPixels;
-
-        mBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-        mCanvas = new Canvas(mBitmap);
-
-        currentColor = DEFAULT_COLOR;
-        strokeWidth = BRUSH_SIZE;
-    }
-
-    public void normal() {
-        emboss = false;
-        blur = false;
-    }
-
-    public void emboss() {
-        emboss = true;
-        blur = false;
-    }
-
-    public void blur() {
-        emboss = false;
-        blur = true;
-    }
-
-    public void clear() {
-        backgroundColor = DEFAULT_BG_COLOR;
-        paths.clear();
-        normal();
-        invalidate();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        canvasView = (CanvasView) findViewById(R.id.canvasView);
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        canvasView.init(metrics);
     }
 
     @Override
-    protected void onDraw(Canvas canvas) {
-        canvas.save();
-        mCanvas.drawColor(backgroundColor);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
-        for (Brush fp : paths) {
-            mPaint.setColor(fp.color);
-            mPaint.setStrokeWidth(fp.strokeW);
-            mPaint.setMaskFilter(null);
-
-            if (fp.emboss)
-                mPaint.setMaskFilter(mEmboss);
-            else if (fp.blur)
-                mPaint.setMaskFilter(mBlur);
-
-            mCanvas.drawPath(fp.path, mPaint);
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.normal:
+                canvasView.normal();
+                return true;
+            case R.id.emboss:
+                canvasView.emboss();
+                return true;
+            case R.id.blur:
+                canvasView.blur();
+                return true;
+            case R.id.clear:
+                canvasView.clear();
+                return true;
         }
 
-        canvas.drawBitmap(mBitmap, 0, 0, mBitmapPaint);
-        canvas.restore();
+        return super.onOptionsItemSelected(item);
     }
 }
